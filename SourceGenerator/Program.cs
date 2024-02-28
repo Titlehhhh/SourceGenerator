3ae4ce7220d378d13e1df51a819e88f6d64129a3
TitleHHHH <TitleHHHH@outlook.com>
2024-02-28 21:38:24 +0500

    Добавьте файлы проекта.

---
+++ SourceGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+using System.Linq;
+
+static string ConvertToPascalCase(string str)
+{
+	TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+	return textInfo.ToTitleCase(str.Replace("_", " ")).Replace(" ", "");
+}
+
+using var sr = new StreamReader("protocol.json");
+
+dynamic json = JObject.Parse(sr.ReadToEnd());
+
+
+JObject clientPackets = json.play.toClient.types;
+
+
+
+foreach (var packet in clientPackets)
+{
+	if (packet.Key.StartsWith("packet_"))
+	{
+		JArray packetWithFields = (JArray)packet.Value.Last;
+		
+	}
+}
+
+Console.ReadLine();
+
