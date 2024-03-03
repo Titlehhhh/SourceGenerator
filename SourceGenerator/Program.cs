d1734af6d322734de4278a09fa1d290f9ec19301
TitleHHHH <TitleHHHH@outlook.com>
2024-03-03 22:28:43 +0500

    add types

--- SourceGenerator/Program.cs
+++ SourceGenerator/Program.cs
@@ -4,34 +4,8 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 
-
- string ToPascalCase(string original)
-{
-	Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
-	Regex whiteSpace = new Regex(@"(?<=\s)");
-	Regex startsWithLowerCaseChar = new Regex("^[a-z]");
-	Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$");
-	Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]");
-	Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))");
-
-	// replace white spaces with undescore, then replace all invalid chars with empty string
-	var pascalCase = invalidCharsRgx.Replace(whiteSpace.Replace(original, "_"), string.Empty)
-		// split by underscores
-		.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
-		// set first letter to uppercase
-		.Select(w => startsWithLowerCaseChar.Replace(w, m => m.Value.ToUpper()))
-		// replace second and all following upper case letters to lower if there is no next lower (ABC -> Abc)
-		.Select(w => firstCharFollowedByUpperCasesOnly.Replace(w, m => m.Value.ToLower()))
-		// set upper case the first lower case following a number (Ab9cd -> Ab9Cd)
-		.Select(w => lowerCaseNextToNumber.Replace(w, m => m.Value.ToUpper()))
-		// lower second and next upper case letters except the last if it follows by any lower (ABcDEf -> AbcDef)
-		.Select(w => upperCaseInside.Replace(w, m => m.Value.ToLower()));
-
-	return string.Concat(pascalCase);
-}
 
 using var sr = new StreamReader("protocol.json");
 
@@ -75,107 +49,21 @@ foreach (var packet in clientPackets)
 
 		Packet pack = new();
 
-		pack.Name = "Client" + ToPascalCase(packet.Key.Substring(7)) + "Packet";
+		//pack.Name = "Client" + ToPascalCase(packet.Key.Substring(7)) + "Packet";
 
 		foreach (var field in packetWithFields)
 		{
-			string name = ToPascalCase(field.Value<string>("name"));
+			string name = field.Value<string>("name");
 			var type = field.Value<JToken>("type");
 
-			if (type.Type != JTokenType.String)
-			{
 
-				ok = false;
-				continue;
-			}
-			if (!supportedTypes.Contains(type.ToString()))
-			{
-				ok = false;
-				continue;
-			}
 
-			pack.Fields.Add(name, type.ToString());
+			
 		}
 
-		if (ok)
-		{
-			packets.Add(pack);
-		}
 	}
 }
 
-
-Dictionary<string, TypeMap> typeToType = new Dictionary<string, TypeMap>
-{
-	{"varint", new("int", "ReadVarInt","WriteVarInt")},
-	{"varlong", new("long", "ReadVarLong","WriteVarLong") },
-	{"u16", new("ushort", "ReadUnsignedShort","WriteUnsignedShort") },
-	{"u8",  new("byte", "ReadUnsignedByte","WriteUnsignedByte") },
-	{"i64", new("long", "ReadLong","WriteLong") },
-	{"i32", new("int", "ReadInt","WriteInt") },
-	{"i8",  new("sbyte", "ReadByte","WriteByte") },
-	{"bool",new("bool", "ReadBoolean","WriteBoolean") },
-	{"i16", new("short", "ReadShort","WriteShort") },
-	{"f32",new("float", "ReadFloat","WriteFloat") },
-	{"f64", new("double" , "ReadDouble","WriteDouble")},
-};
-
-
-string template = await File.ReadAllTextAsync("PacketTemplate.cs");
-
-
-if (Directory.Exists("outputPackets"))
-{
-	Directory.Delete("outputPackets", true);
-	Directory.CreateDirectory("outputPackets");
-}
-else
-{
-	Directory.CreateDirectory("outputPackets");
-}
-
-List<Task> tasks = new();
-
-foreach (var packet in packets)
-{
-
-
-
-	StringBuilder propsBuilder = new();
-	StringBuilder readBuilder = new();
-	StringBuilder writeBuilder = new();
-
-	foreach (var field in packet.Fields)
-	{
-		TypeMap map = typeToType[field.Value];
-
-		string prop = $"\t\tpublic {map.NET_Name} {field.Key} {{ get; set; }}";
-
-		string read = $"\t\t\t{field.Key} = reader.{map.ReadMethod}();";
-
-		string write = $"\t\t\twriter.{map.WriteMethod}({field.Key});";
-
-		propsBuilder.AppendLine(prop);
-		readBuilder.AppendLine(read);
-		writeBuilder.AppendLine(write);
-
-	}
-
-
-	string source = string.Format(
-		template,
-		packet.Name,
-		readBuilder.ToString().Trim(),
-		writeBuilder.ToString().Trim(),
-		propsBuilder.ToString().Trim());
-
-
-	var t = File.WriteAllTextAsync(Path.Combine("outputPackets", $"{packet.Name}.cs"), source);
-	tasks.Add(t);
-
-}
-
-await Task.WhenAll(tasks);
 
 
 public class TypeMap
---
+++ SourceGenerator/ProtoDefTypes/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceGenerator.ProtoDefTypes
+{
+	public class Numeric
+	{
+		public int Size { get; }
+		public bool Signed { get; }
+		public string NET_Name { get; }
+		public string Name { get; }
+
+		public Numeric(string name, string dotnet_name, int size, bool signed)
+		{
+			Size = size;
+			Signed = signed;
+			NET_Name = dotnet_name;
+			Name = name;
+		}
+
+
+	}
+	public static class Numerics
+	{
+		public static Dictionary<string, Numeric> All = new Dictionary<string, Numeric>
+		{
+			{"i8", new("i8", "sbyte", 1, true) },
+			{"u8", new("u8", "byte", 1, false) },
+			{"i16", new("i16", "sbyte", 2, true) },
+			{"u16", new("u16", "ushort", 2, true) },
+			{"i32", new("i32", "sbyte", 4, true) },
+			{"u32", new("u32", "uint", 4, false) },
+			{"f32", new("f32", "float", 4, true) },
+			{"f64", new("f64", "double", 8, true) },
+			{"i64", new("i64", "long", 8, true) },
+			{"u64", new("u64", "ulong", 8, false) },
+		};
+	}
+
+	public class PSwitch
+	{
+		public string CompareTo { get; set; }
+		public string? CompareToValue { get; set; }
+
+		public IEnumerable<Field> Fields { get; set; }
+
+		public string? Default { get; set; }
+	}
+	public class Option
+	{
+		public PType Type { get; set; }
+	}
+	public class PContainer : Collection<Field>
+	{
+		
+	}
+
+	public class PArray
+	{
+		public PType Type { get; set; }
+		public PType CountType { get; set; }
+
+		public string? Count { get; set; }
+	}
+	public class PType
+	{
+
+	}
+	public class Field
+	{
+		public string Name { get; set; }
+		public PType Type { get; set; }
+	}
+
+	public class PBuffer
+	{
+		public PType CountType { get; set; }
+		public string? Count { get; set; }
+		public bool? Rest { get; set; }
+	}
+
+	public class BitField : Collection<BitFieldNode>
+	{
+	}
+	public class BitFieldNode
+	{
+		public string Name { get; set; }
+		public int Size { get; set; }
+		public bool Signed { get; set; }
+	}
+	public class Mapper
+	{
+
+	}
+	public class PString
+	{
+
+	}
+
+
+}
